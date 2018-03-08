763a6b67d1d06de14115e5f4e0683055225d02a2
Rafał Pocztarski <r.pocztarski@gmail.com>
2018-03-08 06:28:23 +0100

    Add circular reference check

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const getGlobalObject = () => {
-  if (typeof self !== 'undefined') {
+  if (typeof self !== 'undefined' && self.self === self) {
     return self;
   }
-  if (typeof window !== 'undefined') {
+  if (typeof window !== 'undefined' && window.window === self) {
     return window;
   }
-  if (typeof global !== 'undefined') {
+  if (typeof global !== 'undefined' && global.global === global) {
     return global;
   }
   throw new Error('Cannot find the global object');
