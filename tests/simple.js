e7cb2ddbcc59d57f2c032793cf99485ee4140c1a
Rafał Pocztarski <r.pocztarski@gmail.com>
2018-03-08 06:19:52 +0100

    Add simple tests

---
+++ tests/simple.js
@@ -0,0 +1,14 @@
+const tap = require('tap');
+
+const g = require('..');
+
+tap.test('simple tests', (t) => {
+  t.plan();
+
+  t.equal(typeof xxx, 'undefined', 'xxx should not be defined');
+
+  g.xxx = {};
+
+  t.notEqual(typeof xxx, 'undefined', 'xxx should be defined');
+  t.equal(xxx, g.xxx, 'xxx should habe the right value');
+});
