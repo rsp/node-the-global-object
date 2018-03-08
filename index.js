353e584ab8388bd9fdc4e6c51148433513cea17b
Rafał Pocztarski <r.pocztarski@gmail.com>
2018-03-08 06:37:54 +0100

    Add tests for expected properties

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 'use strict';
 
 const getGlobalObject = () => {
-  if (typeof self !== 'undefined' && self.self === self) {
+  if (typeof self !== 'undefined' && self.self === self &&
+    self.Array === Array && self.setInterval === setInterval) {
     return self;
   }
-  if (typeof window !== 'undefined' && window.window === self) {
+  if (typeof window !== 'undefined' && window.window === window &&
+    window.Array === Array && window.setInterval === setInterval) {
     return window;
   }
-  if (typeof global !== 'undefined' && global.global === global) {
+  if (typeof global !== 'undefined' && global.global === global &&
+    global.Array === Array && global.setInterval === setInterval) {
     return global;
   }
   throw new Error('Cannot find the global object');
